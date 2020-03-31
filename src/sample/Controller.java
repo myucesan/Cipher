babe563c4a1671020fae4a849a9a4ba55d617c66
= <=>
2020-03-31 11:39:11 +0200

    step 1 setup

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import com.sun.xml.internal.ws.api.FeatureConstructor;
+import javafx.fxml.FXML;
+
+import javafx.*;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    javafx.scene.control.Button Button;
+    @FXML
+    TextField n;
+    @FXML
+    TextField generatedPForN;
+
+    @FXML
+    TextField generatedQForN;
+
+    @FXML
+    private void s1_CalculatePandQ() {
+        Integer number = Integer.parseInt(n.getText());
+        generatedPForN.setText(number.toString());
+        generatedQForN.setText(number.toString());
+
+        // perform Euclids algorithm on "number" and that results in p and q.
+
+    }
 }
