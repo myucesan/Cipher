1e2c5f1d769ef36341c8cdd31a87f134ec6673a7
= <=>
2020-04-02 21:21:38 +0200

    finished step 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,14 @@ import javafx.fxml.FXML;
 import javafx.*;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class Controller {
 
     @FXML
     javafx.scene.control.Button Button;
     @FXML
-    TextField n;
+    TextField givenN;
     @FXML
     TextField generatedPForN;
 
@@ -20,11 +22,28 @@ public class Controller {
 
     @FXML
     private void s1_CalculatePandQ() {
-        Integer number = Integer.parseInt(n.getText());
-        generatedPForN.setText(number.toString());
-        generatedQForN.setText(number.toString());
+        /* perform Euclids (prime factorization) algorithm on "number" and that results in p and q.
+        Step 1: Take the given N and extract p and q using Euclid's algorithm
+                Note the time it takes to produce p and q.
+                TODO: Check if we used Euclid or if Euclid is actually relevant for this case.
+         */
 
-        // perform Euclids algorithm on "number" and that results in p and q.
+        Integer n = Integer.parseInt(givenN.getText());
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+
+        for(int i = 2; i < n; i++) {
+            while(n % i == 0) {
+                primeNumbers.add(i);
+                n = n / i;
+            }
+        }
+
+        if(n > 2) {
+            primeNumbers.add(n);
+        }
+
+        generatedQForN.setText(primeNumbers.get(0).toString());
+        generatedPForN.setText(primeNumbers.get(1).toString());
 
     }
 }
